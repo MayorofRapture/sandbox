f2540d67f1f6ffaf266394a5496fe8202d856814
MayorofRapture <57378695+MayorofRapture@users.noreply.github.com>
2020-06-01 20:40:30 -0400

    Add files via upload

---
+++ staminaBar.js
@@ -0,0 +1,41 @@
+// The purpose of this code is to amimate the progress bars 
+
+let i = 0;
+let progressBar = $("#Stamina");
+// color variables
+let red = 166;
+let green = 166;
+let blue = 166;
+
+
+
+function countNumbers(){
+    // Change the integer to desired percentage
+    if(i < 65){
+        i = i + 1;
+        progressBar.css("width", i + "%");
+        checkNumbers();
+    }
+    
+    // Wait for sometime before running this script again
+    setTimeout("countNumbers()", 10);
+}
+function checkNumbers(){
+    // if i is divisible by 5 the color changes
+    if((i % 5 === 0) && (i > 4)){
+        changeColor();
+    }    
+}
+function changeColor(){
+    // As the progress bar loads the color changes
+    // Changes the rgb value
+    red = red - 4;
+    green = green + 4;
+    blue = blue + 4;
+    // Apply changes to the css
+    progressBar.css({
+        background: "rgb(" + red + "," + green + "," + blue + ")"
+    })
+}
+
+countNumbers();
